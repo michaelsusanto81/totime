733578d833f2ac6c85e4d277c649ecb48e163416
Michael Susanto <michaelsusanto32@gmail.com>
2020-11-12 14:04:19 +0700

    Implement take picture intent and save to Room

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/totime/ui/profile/ProfileActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/totime/ui/profile/ProfileActivity.kt
@@ -1,10 +1,20 @@
 package id.ac.ui.cs.mobileprogramming.michaelsusanto.totime.ui.profile
 
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,13 +23,21 @@ import androidx.navigation.ui.setupWithNavController
 import id.ac.ui.cs.mobileprogramming.michaelsusanto.totime.R
 import id.ac.ui.cs.mobileprogramming.michaelsusanto.totime.data.model.User
 import id.ac.ui.cs.mobileprogramming.michaelsusanto.totime.databinding.ActivityProfileBinding
+import kotlinx.io.IOException
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProfileBinding
     private lateinit var viewModel: ProfileViewModel
     private lateinit var viewModelFactory: ProfileViewModelFactory
+    private lateinit var currentPhotoPath: String
     private val INVALID_INPUT = "Please fill all fields."
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_TAKE_PHOTO = 1
+    private var currentUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,11 +64,14 @@ class ProfileActivity : AppCompatActivity() {
     private fun updateView(user: User) {
         binding.nameLabel.text = user.name
         binding.emailLabel.text = user.email
+        if (user.profPic != "") {
+            val uri = Uri.parse(user.profPic)
+            binding.profPic.setImageURI(null)
+            binding.profPic.setImageURI(uri)
+        }
     }
 
-    private fun edit() {
-        val name = viewModel.liveData.value?.name
-        val email = viewModel.liveData.value?.email
+    private fun show() {
         binding.nameLabel.visibility = View.GONE
         binding.emailLabel.visibility = View.GONE
         binding.btnEdit.visibility = View.GONE
@@ -60,30 +81,9 @@ class ProfileActivity : AppCompatActivity() {
         binding.btnCancel.visibility = View.VISIBLE
         binding.btnCamera.visibility = View.VISIBLE
         binding.btnGallery.visibility = View.VISIBLE
-        binding.nameEdit.setText(name)
-        binding.emailEdit.setText(email)
     }
 
-    private fun save() {
-        val name = binding.nameEdit.text.toString()
-        val email = binding.emailEdit.text.toString()
-        val user = viewModel.liveData.value
-        if(!viewModel.validateInput(name, email)) {
-            val toast = Toast.makeText(this, INVALID_INPUT, Toast.LENGTH_LONG)
-            toast.show()
-        } else {
-            if (user != null) {
-                user.name = name
-                user.email = email
-                viewModel.updateData(user)
-            }
-            binding.nameLabel.text = name
-            binding.emailLabel.text = email
-            cancel()
-        }
-    }
-
-    private fun cancel() {
+    private fun hide() {
         binding.nameLabel.visibility = View.VISIBLE
         binding.emailLabel.visibility = View.VISIBLE
         binding.btnEdit.visibility = View.VISIBLE
@@ -95,13 +95,144 @@ class ProfileActivity : AppCompatActivity() {
         binding.btnGallery.visibility = View.GONE
     }
 
-    private fun camera() {
+    private fun edit() {
+        val name = viewModel.liveData.value?.name
+        val email = viewModel.liveData.value?.email
+        show()
+        binding.nameEdit.setText(name)
+        binding.emailEdit.setText(email)
+    }
 
+    private fun save() {
+        val name = binding.nameEdit.text.toString()
+        val email = binding.emailEdit.text.toString()
+        val user = viewModel.liveData.value
+        if (!viewModel.validateInput(name, email)) {
+            val toast = Toast.makeText(this, INVALID_INPUT, Toast.LENGTH_LONG)
+            toast.show()
+        } else {
+            if (user != null) {
+                user.name = name
+                user.email = email
+                if (currentUri != null) {
+                    user.profPic = currentUri.toString()
+                }
+                viewModel.updateData(user)
+            }
+            binding.nameLabel.text = name
+            binding.emailLabel.text = email
+            hide()
+        }
+    }
+
+    private fun cancel() {
+        hide()
+        val user = viewModel.liveData.value
+        if (user != null) {
+            if (user.profPic != "") {
+                val uri = Uri.parse(user.profPic)
+                binding.profPic.setImageURI(null)
+                binding.profPic.setImageURI(uri)
+            } else {
+                binding.profPic.setImageResource(R.drawable.ic_person)
+            }
+        }
+    }
+
+    private fun camera() {
+        dispatchTakePictureIntent()
     }
 
     private fun gallery() {
 
     }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "id.ac.ui.cs.mobileprogramming.michaelsusanto.totime.fileprovider",
+                        it
+                    )
+                    currentUri = photoURI
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        val targetW: Int = binding.profPic.width
+        val targetH: Int = binding.profPic.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            BitmapFactory.decodeFile(currentPhotoPath, this)
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+            val scaleFactor: Int =
+                1.coerceAtLeast((photoW / targetW).coerceAtMost(photoH / targetH))
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inPurgeable = true
+        }
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
+            binding.profPic.setImageBitmap(bitmap)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            galleryAddPic()
+            setPic()
+        }
+    }
+
 
     private fun setupToolbar() {
         setSupportActionBar(binding.toolbar)
