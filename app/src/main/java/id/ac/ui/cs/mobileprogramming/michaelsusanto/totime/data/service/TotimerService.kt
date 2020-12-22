e3396feb653a058908e4661799c1bc3ca1bb50b3
Michael Susanto <michaelsusanto32@gmail.com>
2020-12-22 22:17:51 +0700

    add notification to show timer progress and after stop timer

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/totime/data/service/TotimerService.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/totime/data/service/TotimerService.kt
@@ -1,9 +1,15 @@
 package id.ac.ui.cs.mobileprogramming.michaelsusanto.totime.data.service
 
-import android.app.Service
+import android.app.*
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Handler
 import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.totime.MainActivity
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.totime.R
 
 class TotimerService: Service() {
 
@@ -26,10 +32,16 @@ class TotimerService: Service() {
     private var minutes: String = ""
     private var seconds: String = ""
 
+    private val ONGOING_NOTIFICATION_ID = 1
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+    private lateinit var notificationManager: NotificationManager
+
     private val startTimer: Runnable = object : Runnable {
         override fun run() {
             elapsedTime = System.currentTimeMillis() - startTime
             updateTimer(elapsedTime)
+            notificationBuilder.setContentText("$hours:$minutes:$seconds")
+            notificationManager.notify(ONGOING_NOTIFICATION_ID, notificationBuilder.build())
             mHandler.postDelayed(this, REFRESH_RATE)
         }
     }
@@ -62,10 +74,52 @@ class TotimerService: Service() {
 
         mHandler.removeCallbacks(startTimer)
         mHandler.postDelayed(startTimer, 0)
+
+        buildNotification()
+    }
+
+    private fun buildNotification() {
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("totimer_service", "Totimer Service")
+            } else {
+                // If earlier version channel ID is not used
+                // https://developer.android.com/reference/android/support/v4/app/NotificationCompat.Builder.html#NotificationCompat.Builder(android.content.Context)
+                ""
+            }
+
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(this, 0, notificationIntent, 0)
+            }
+
+        notificationBuilder = NotificationCompat.Builder(this,
+            channelId
+        )
+            .setContentTitle(getText(R.string.totimer))
+            .setContentText("$hours:$minutes:$seconds")
+            .setSmallIcon(R.drawable.ic_totimer)
+            .setContentIntent(pendingIntent)
+            .setCategory(Notification.CATEGORY_SERVICE)
+
+        startForeground(ONGOING_NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String{
+        val chan = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(chan)
+        return channelId
     }
 
     override fun onDestroy() {
         mHandler.removeCallbacks(startTimer)
+        stopForeground(true)
+        stopSelf()
         stopped = true
         super.onDestroy()
     }
